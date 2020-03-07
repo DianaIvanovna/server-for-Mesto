0bac2b44995706e05356baf21c707e51f3a0c7b0
Diana Garbuz <GDI.1999@yandex.ru>
2020-03-07 19:53:04 +0300

    сделала доступ по умолчанию в папку public

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
+const path = require('path');
 
-const app=express();
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT);

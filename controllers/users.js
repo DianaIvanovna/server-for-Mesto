bacea872bc9e2046f71e5416105ac92e3c26d871
Diana Garbuz <GDI.1999@yandex.ru>
2020-04-05 09:52:15 +0300

    install module bcryptjs + edit createUser

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
@@ -19,8 +20,11 @@ module.exports.userSearch = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const { name, about, avatar, email, password } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash => User.create({
+      name, about, avatar, email, password: hash,
+    })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send(err.message));
 };
